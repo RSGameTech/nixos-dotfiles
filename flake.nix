c759e2ecc5ea0ddda8a8d7c008239eb660028f62
RSGameTech <rsgame0604@gmail.com>
2025-05-30 00:36:37 +0530

    bruh

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @inputs: {
+    nixosConfigurations = {
+      loq15iax9 = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+        ]
+      }
+    }
+  }
+}
