2002553ce830851349387cca2e643ccbbf6a80a7
RSGameTech <rsgame0604@gmail.com>
2025-06-01 01:56:26 +0530

    bruh

--- flake.nix
+++ flake.nix
@@ -1,22 +1,37 @@
 {
-  description = "A very basic flake";
+  description = "My own NixOS Configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  #   quickshell = {
+  #     url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+  #     inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
     ...
-  } @inputs: {
+  } @inputs: let
+    inherit (self) outputs;
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = fn: nixpkgs.lib.genAttrs systems (
+      system: fn (import nixpkgs { inherit system; })
+    );
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+
+    packages = forAllSystems (pkgs: {
+      defaults = pkgs.hello;
+    });
+
     nixosConfigurations = {
       loq15iax9 = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
+        specialArgs = { inherit inputs outputs; };
         modules = [
           ./configuration.nix
-        ]
-      }
-    }
-  }
+        ];
+      };
+    };
+  };
 }
