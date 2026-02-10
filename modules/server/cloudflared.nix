e1d87425b89f2eb7f58c3aa06fa2c4d24841b9a9
RSGameTech <rsgame0604@gmail.com>
2026-02-11 05:08:08 +0530

    Great :) Cloudflare is working

--- modules/server/cloudflared.nix
+++ modules/server/cloudflared.nix
@@ -12,18 +12,18 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    age.secrets.cloudflaredTunnel = {
-      file = ../../secrets/cloudflared-tunnel.age;
-      mode = "600";
-    }; 
+    #age.secrets.cloudflaredTunnel = {
+    #  file = ../../secrets/cloudflared-tunnel.age;
+    #  mode = "600";
+    #}; 
     services.cloudflared = {
       enable = true;
       tunnels = {
         "2ea3b9c4-7e25-4a7b-84a0-01a894faffa8" = {
-          credentialsFile = config.age.secrets.cloudflaredTunnel.path;
+          credentialsFile = "/etc/cloudflared/2ea3b9c4-7e25-4a7b-84a0-01a894faffa8.json";
           ingress = {
-            "glance.rsgametech.com" = {
-              service = "http://localhost:8800";
+            "glance.rsgametech.me" = {
+              service = "http://127.0.0.1:8800";
             };
           };
           default = "http_status:404";
